1264f1e163ac6082d6dfdd4365e1e9c730c057ba
jakelangton <planktonlangton@gmail.com>
2020-04-07 17:11:57 +1000

    Test

--- Models/Main.py
+++ Models/Main.py
@@ -1,4 +1,4 @@
-print("Test")
+
 # import tensorflow
 # import keras
 # from keras.datasets import fashion_mnist
@@ -6,6 +6,6 @@ print("Test")
 # (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 #
 
-import mnist_reader
+i)mport mnist_reader
 X_train, y_train = mnist_reader.load_mnist('data/fashion', kind='train')
-X_test, y_test = mnist_reader.load_mnist('data/fashion', kind='t10k')
+X_test, y_test = mnist_reader.load_mnist('data/fashion', kind='t10k'
